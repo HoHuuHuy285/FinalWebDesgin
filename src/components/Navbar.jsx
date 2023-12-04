3f13cac56fbc6789200795d3a44c41ad04021a81
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2023-12-05 01:03:47 +0700

    Sun Moon Theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,32 @@ import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import NavLinks from "./Navlinks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// Thiết lập đối tượng chủ đề
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem("theme") || themes.winter;
+};
 
 const Navbar = () => {
   // Chuyển chế độ nền tối và sáng
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(getThemeFromLocalStorage);
   const handleTheme = () => {
-    setTheme(!theme);
+    const { winter, dracula } = themes;
+    const newTheme = theme === winter ? dracula : winter;
+    document.documentElement.setAttribute("data-theme", theme);
+    setTheme(newTheme);
   };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element ">
