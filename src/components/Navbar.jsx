5b7fb7d21c8f0c998192fd29c1a7f31dd2b2fed2
HoHuuHuy285 <huy.ho210215@vnuk.edu.vn>
2023-12-05 23:30:56 +0700

    Create and Update Cart Item. List

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import NavLinks from "./Navlinks";
 import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 // Thiết lập đối tượng chủ đề
 const themes = {
@@ -28,6 +29,9 @@ const Navbar = () => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
   }, [theme]);
+
+  const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
+
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element ">
@@ -71,7 +75,7 @@ const Navbar = () => {
             <div className="indicator">
               <BsCart3 className="h-6 w-6" />
               <span className="badge badge-sm badge-primary indicator-item">
-                8
+                {numItemsInCart}
               </span>
             </div>
           </NavLink>
