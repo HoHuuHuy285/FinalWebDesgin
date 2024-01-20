86489507c2ed209cf7926eb91364f8bd155c47a3
Thuannek <thuan.le210219@vnuk.edu.vn>
2024-01-20 15:31:25 +0700

    up

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -11,6 +11,9 @@ const CartItem = ({ cartItem }) => {
     dispatch(editItem({ cartID, amount: parseInt(e.target.value) }));
   };
 
+
+  
+
   const { cartID, title, price, image, amount, company, productColor } =
     cartItem;
 
